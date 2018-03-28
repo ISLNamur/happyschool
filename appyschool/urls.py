0deff9a61ae39a35dcb817df59c05457991afe54
Supermanu <manueltondeur@gmail.com>
2018-03-28 16:35:29 +0200

    All: Add AGPL licence

--- appyschool/urls.py
+++ appyschool/urls.py
@@ -1,18 +1,22 @@
-"""appyschool URL Configuration
+# This file is part of Appyschool.
+#
+# Appyschool is the legal property of its developers, whose names
+# can be found in the AUTHORS file distributed with this source
+# distribution.
+#
+# Appyschool is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Appyschool is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Appyschool.  If not, see <http://www.gnu.org/licenses/>.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
--- schedule_change/views.py
+++ schedule_change/views.py
@@ -1,16 +1,36 @@
-from rest_framework.renderers import JSONRenderer
+# This file is part of Appyschool.
+#
+# Appyschool is the legal property of its developers, whose names
+# can be found in the AUTHORS file distributed with this source
+# distribution.
+#
+# Appyschool is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Appyschool is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Appyschool.  If not, see <http://www.gnu.org/licenses/>.
 
-from django.shortcuts import render
 
-from .models import ScheduleChangeSettings
-from .serializers import ScheduleChangeSettingsSerializer
+# from rest_framework.renderers import JSONRenderer
+#
+# from django.shortcuts import render
 
-
-def test(request):
-    try:
-        settings = ScheduleChangeSettings.objects.all()[0]
-        json_settings = JSONRenderer().render(ScheduleChangeSettingsSerializer(settings).data)
-    except IndexError:
-        json_settings = {}
-
-    return render(request, template_name="schedule_change/index.html", context={'app_settings': json_settings})
+# from .models import ScheduleChangeSettings
+# from .serializers import ScheduleChangeSettingsSerializer
+#
+#
+# def test(request):
+#     try:
+#         settings = ScheduleChangeSettings.objects.all()[0]
+#         json_settings = JSONRenderer().render(ScheduleChangeSettingsSerializer(settings).data)
+#     except IndexError:
+#         json_settings = {}
+#
+#     return render(request, template_name="schedule_change/index.html", context={'app_settings': json_settings})
