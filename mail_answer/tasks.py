c6ae1ab28f992c42b2dba7286d7b9f8113d42a83
Supermanu <manueltondeur@gmail.com>
2018-05-17 11:38:43 +0200

    Mail_answer: Add missing tasks file

---
+++ mail_answer/tasks.py
@@ -0,0 +1,30 @@
+# This file is part of HappySchool.
+#
+# HappySchool is the legal property of its developers, whose names
+# can be found in the AUTHORS file distributed with this source
+# distribution.
+#
+# HappySchool is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# HappySchool is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with HappySchool.  If not, see <http://www.gnu.org/licenses/>.
+
+import subprocess
+
+from celery import shared_task
+
+from django.conf import settings
+
+
+@shared_task(bind=True)
+def task_sync_mail_answers(self):
+    subprocess.run(settings.MAIL_ANSWER['template_sync'], shell=True)
+    subprocess.run(settings.MAIL_ANSWER['mail_answers_create'], shell=True)
