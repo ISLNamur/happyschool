f40c2265fd09a8d0036073ae60190e87d1c3ec05
Stan <39907380+Stan-Devs@users.noreply.github.com>
2018-06-18 14:47:06 +0200

    Create Migration.sh

---
+++ Script/Migration.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+cd /home/happyschool/happyschool
+
+yes |python3 manage.py makemigrations annuaire
+yes |python3 manage.py migrate
+
+yes |python3 manage.py makemigrations absence_prof
+yes |python3 manage.py migrate
+
+yes |python3 manage.py makemigrations admin
+yes |python3 manage.py migrate
+
+yes |python3 manage.py makemigrations appels
+yes |python3 manage.py migrate
+
+yes |python3 manage.py makemigrations auth
+yes |python3 manage.py migrate
+
+yes |python3 manage.py makemigrations authtoken
+yes |python3 manage.py migrate
+
+yes |python3 manage.py makemigrations contenttypes
+yes |python3 manage.py migrate
+
+yes |python3 manage.py makemigrations core
+yes |python3 manage.py migrate
+
+yes |python3 manage.py makemigrations dossier_eleve
+yes |python3 manage.py migrate
+
+yes |python3 manage.py makemigrations infirmerie
+yes |python3 manage.py migrate
+
+yes |python3 manage.py makemigrations mail_answer
+yes |python3 manage.py migrate
+
+yes |python3 manage.py makemigrations mail_notification
+yes |python3 manage.py migrate
+
+yes |python3 manage.py makemigrations sessions
+yes |python3 manage.py migrate
+
+
+
