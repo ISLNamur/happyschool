ecfbcc29bc9e14f3521f8172174c825a023e001b
Supermanu <manueltondeur@gmail.com>
2018-06-21 11:21:56 +0200

    Dossier_eleve: Add important filter

--- dossier_eleve/views.py
+++ dossier_eleve/views.py
@@ -742,16 +742,18 @@ class DossierEleveView(BaseDossierEleveView):
         {'value': 'info__info', 'text': 'Info'},
         {'value': 'sanction_decision__sanction_decision', 'text': 'Sanction/décision'},
         {'value': 'datetime_encodage', 'text': 'Date encodage'},
+        {'value': 'activate_important', 'text': 'Important'},
         {'value': 'matricule_id', 'text': 'Matricule'},
         {'value': 'scholar_year', 'text': 'Année scolaire'},
     ]
 
 class CasEleveFilter(BaseFilters):
     classe = filters.CharFilter(method='classe_by')
+    activate_important = filters.BooleanFilter(name="important")
 
     class Meta:
         fields_to_filter = ('name', 'matricule_id', 'info__info', 'sanction_decision__sanction_decision',
-                            'datetime_encodage', 'scholar_year', 'important')
+                            'datetime_encodage', 'scholar_year')
         model = CasEleve
         fields = BaseFilters.Meta.generate_filters(fields_to_filter)
         filter_overrides = BaseFilters.Meta.filter_overrides
