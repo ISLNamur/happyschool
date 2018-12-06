9b6d8b02f133df982ccbb28b357630fc406703dc
Supermanu <manueltondeur@gmail.com>
2018-12-06 17:08:06 +0100

    Mail_answer: Use babel polyfill from bundle

--- assets/js/answer.js
+++ assets/js/answer.js
@@ -17,8 +17,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with Happyschool.  If not, see <http://www.gnu.org/licenses/>.
 
-import "babel-polyfill";
-
 import Vue from 'vue';
 
 import Answer from '../mail_answer/answer.vue';
