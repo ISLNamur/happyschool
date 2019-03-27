53b2e1bb6e064d0948178644c8744bbfa45855b7
Supermanu <manueltondeur@gmail.com>
2019-03-27 18:36:54 +0100

    SCRIPTS: Remove makemigrations from update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -5,7 +5,6 @@ cd $parent_path
 cd ..
 git pull
 sudo pip3 install -r requirements.txt
-python3 manage.py makemigrations
 python3 manage.py migrate
 npm install
 ./node_modules/.bin/webpack --config webpack.prod.js
