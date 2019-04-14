b6d5922b54f4378591d618b29d14d8b384093eb8
Supermanu <manueltondeur@gmail.com>
2019-04-14 23:53:30 +0200

    STUDENT_ABSENCE: Fix migration dependency

--- student_absence/migrations/0001_initial.py
+++ student_absence/migrations/0001_initial.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('core', '0003_coresettingsmodel_root'),
+        ('core', '0001_initial'),
     ]
 
     operations = [
