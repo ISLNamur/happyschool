3f60e48cf570193d406b8c9b05bbf05a7c53ed46
Manu <manueltondeur@gmail.com>
2019-07-09 15:46:20 +0200

    SCRIPTS: Install python packages in user folder

--- scripts/update.sh
+++ scripts/update.sh
@@ -4,7 +4,7 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd $parent_path
 cd ..
 git pull
-sudo pip3 install -r requirements.txt
+pip3 install --user -r requirements.txt
 python3 manage.py migrate
 npm install
 ./node_modules/.bin/webpack --config webpack.prod.js
