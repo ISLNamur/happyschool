c62d830844e8244d4c7ce5cc8423525a69e254ce
Supermanu <manueltondeur@gmail.com>
2019-07-10 12:31:36 +0200

    STUDENT_ABSENCE: Add absence list

---
+++ assets/student_absence/absence_entry.vue
@@ -0,0 +1,57 @@
+<!-- This file is part of Happyschool. -->
+<!--  -->
+<!-- Happyschool is the legal property of its developers, whose names -->
+<!-- can be found in the AUTHORS file distributed with this source -->
+<!-- distribution. -->
+<!--  -->
+<!-- Happyschool is free software: you can redistribute it and/or modify -->
+<!-- it under the terms of the GNU Affero General Public License as published by -->
+<!-- the Free Software Foundation, either version 3 of the License, or -->
+<!-- (at your option) any later version. -->
+<!--  -->
+<!-- Happyschool is distributed in the hope that it will be useful, -->
+<!-- but WITHOUT ANY WARRANTY; without even the implied warranty of -->
+<!-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the -->
+<!-- GNU Affero General Public License for more details. -->
+<!--  -->
+<!-- You should have received a copy of the GNU Affero General Public License -->
+<!-- along with Happyschool.  If not, see <http://www.gnu.org/licenses/>. -->
+
+<template>
+    <div>
+        <b-card>
+            <strong>{{ absence.date_absence }}</strong>:
+            <a :href='`/annuaire/#/person/student/${absence.student.matricule}/`'>
+                {{ absence.student.display }}
+            </a>
+             ({{ display_absence }})
+        </b-card>
+    </div>
+</template>
+
+<script>
+import Vue from 'vue';
+
+import 'vue-awesome/icons'
+import Icon from 'vue-awesome/components/Icon.vue'
+Vue.component('icon', Icon);
+
+export default {
+    props: ["absence"],
+    data: function () {
+        return {
+        }
+    },
+    computed: {
+        display_absence: function () {
+            let display = "";
+            if (this.absence.morning) {
+                display += "matin";
+                if (this.absence.afternoon) display += ", ";
+            }
+            if (this.absence.afternoon) display += "après-midi";
+            return display;
+        }
+    }
+}
+</script>
---
+++ assets/student_absence/list.vue
@@ -0,0 +1,95 @@
+<!-- This file is part of Happyschool. -->
+<!--  -->
+<!-- Happyschool is the legal property of its developers, whose names -->
+<!-- can be found in the AUTHORS file distributed with this source -->
+<!-- distribution. -->
+<!--  -->
+<!-- Happyschool is free software: you can redistribute it and/or modify -->
+<!-- it under the terms of the GNU Affero General Public License as published by -->
+<!-- the Free Software Foundation, either version 3 of the License, or -->
+<!-- (at your option) any later version. -->
+<!--  -->
+<!-- Happyschool is distributed in the hope that it will be useful, -->
+<!-- but WITHOUT ANY WARRANTY; without even the implied warranty of -->
+<!-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the -->
+<!-- GNU Affero General Public License for more details. -->
+<!--  -->
+<!-- You should have received a copy of the GNU Affero General Public License -->
+<!-- along with Happyschool.  If not, see <http://www.gnu.org/licenses/>. -->
+
+<template>
+    <div>
+        <b-row>
+            <b-col>
+                <b-card>
+                    <filters app="student_absence" model="student_absence" ref="filters" @update="applyFilter"></filters>
+                </b-card>
+            </b-col>
+        </b-row>
+        <b-row>
+            <b-col>
+                <b-pagination class="mt-1" :total-rows="entriesCount" v-model="currentPage" @change="changePage" :per-page="20">
+                </b-pagination>
+            </b-col>
+        </b-row>
+        <b-row>
+            <b-col>
+                <absence-entry v-for="absence in absences" :key="absence.id" :absence="absence">
+                </absence-entry>
+            </b-col>
+        </b-row>
+        <b-row v-if="entriesCount > 10">
+            <b-col>
+                <b-pagination class="mt-2" :total-rows="entriesCount" v-model="currentPage" @change="changePage" :per-page="20">
+                </b-pagination>
+            </b-col>
+        </b-row>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+import AbsenceEntry from "./absence_entry.vue";
+import Filters from '../common/filters.vue'
+import {getFilters} from '../common/filters.js';
+
+export default {
+    data: function () {
+        return {
+            absences: [],
+            showFilters: true,
+            filter: "",
+            currentPage: 1,
+            entriesCount: 0,
+        }
+    },
+    methods: {
+        changePage: function (page) {
+            this.currentPage = page;
+            this.loadEntries();
+            // Move to the top of the page.
+            scroll(0, 0);
+            return;
+        },
+        applyFilter: function () {
+            this.filter = getFilters(this.$store.state.filters);
+            this.loadEntries();
+        },
+        loadEntries: function () {
+            axios.get('/student_absence/api/student_absence/?ordering=-date_absence' + this.filter + '&page=' + this.currentPage)
+            .then(response => {
+                this.entriesCount = response.data.count;
+                this.absences = response.data.results;
+            });
+        }
+    },
+    mounted: function () {
+        this.applyFilter();
+    },
+    components: {
+        AbsenceEntry,
+        Filters,
+    }
+}
+</script>
