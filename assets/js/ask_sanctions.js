b8419a03f90badac1962ef5211364570c23b351b
Supermanu <manueltondeur@gmail.com>
2019-12-17 15:59:52 +0100

    DOSSIER_ELEVE: Fix style issues

--- assets/js/ask_sanctions.js
+++ assets/js/ask_sanctions.js
@@ -17,51 +17,56 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with Happyschool.  If not, see <http://www.gnu.org/licenses/>.
 
-import Vue from 'vue';
+import Vue from "vue";
 
-import Vuex from 'vuex';
+import Vuex from "vuex";
 Vue.use(Vuex);
 
-import AskSanctions from '../dossier_eleve/askSanctions.vue';
+import AskSanctions from "../dossier_eleve/askSanctions.vue";
 
 const store = new Vuex.Store({
-  state: {
-    settings: settings,
-    filters: [{
-        filterType: 'scholar_year',
-        tag: currentYear,
-        value: currentYear,
-    }],
-    canSetSanction: canSetSanction,
-    canAskSanction: canAskSanction,
-  },
-  mutations: {
-      addFilter: function (state, filter) {
-          // If filter is a matricule, remove name filter to avoid conflict.
-          if (filter.filterType === 'matricule_id') {
-              this.commit('removeFilter', 'name');
-          }
+    state: {
+        // eslint-disable-next-line no-undef
+        settings: settings,
+        filters: [{
+            filterType: "scholar_year",
+            // eslint-disable-next-line no-undef
+            tag: currentYear,
+            // eslint-disable-next-line no-undef
+            value: currentYear,
+        }],
+        // eslint-disable-next-line no-undef
+        canSetSanction: canSetSanction,
+        // eslint-disable-next-line no-undef
+        canAskSanction: canAskSanction,
+    },
+    mutations: {
+        addFilter: function (state, filter) {
+            // If filter is a matricule, remove name filter to avoid conflict.
+            if (filter.filterType === "matricule_id") {
+                this.commit("removeFilter", "name");
+            }
 
-          // Overwrite same filter type.
-          this.commit('removeFilter', filter.filterType);
+            // Overwrite same filter type.
+            this.commit("removeFilter", filter.filterType);
 
-          state.filters.push(filter);
-      },
-      removeFilter: function (state, key) {
-          for (let f in state.filters) {
-              if (state.filters[f].filterType === key) {
-                  state.filters.splice(f, 1);
-                  break;
-              }
-          }
-      }
-  }
+            state.filters.push(filter);
+        },
+        removeFilter: function (state, key) {
+            for (let f in state.filters) {
+                if (state.filters[f].filterType === key) {
+                    state.filters.splice(f, 1);
+                    break;
+                }
+            }
+        }
+    }
 });
 
-var askSanctionsApp = new Vue({
-    el: '#vue-app',
+new Vue({
+    el: "#vue-app",
     data: {},
     store,
-    template: '<ask-sanctions/>',
+    template: "<ask-sanctions/>",
     components: { AskSanctions },
-})
+});
