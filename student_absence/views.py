9d5b1ff84ee31c1116def4689d2af358920c8406
Supermanu <manueltondeur@gmail.com>
2020-02-19 14:41:47 +0100

    STUDENT_ABS: Enable multiple classe filtering

--- student_absence/views.py
+++ student_absence/views.py
@@ -100,16 +100,6 @@ class StudentAbsenceFilter(BaseFilters):
         fields = BaseFilters.Meta.generate_filters(fields_to_filter)
         filter_overrides = BaseFilters.Meta.filter_overrides
 
-    def classe_by(self, queryset, field_name, value):
-        if not value[0].isdigit():
-            return queryset
-
-        if len(value) > 0:
-            queryset = queryset.filter(student__classe__year=value[0])
-            if len(value) > 1:
-                queryset = queryset.filter(student__classe__letter__istartswith=value[1:])
-        return queryset
-
     def activate_is_absent_by(self, queryset, name, value):
         return queryset.filter(is_absent=True)
 
