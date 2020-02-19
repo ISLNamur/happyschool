fa13b9351863a96f108d680ac57f79cab276e59a
Supermanu <manueltondeur@gmail.com>
2020-02-19 14:55:24 +0100

    DOSSIER_ELEVE: Remove commented code

--- dossier_eleve/views.py
+++ dossier_eleve/views.py
@@ -151,22 +151,6 @@ class CasEleveFilter(BaseFilters):
         fields = BaseFilters.Meta.generate_filters(fields_to_filter)
         filter_overrides = BaseFilters.Meta.filter_overrides
 
-    # def classe_by(self, queryset, field_name, value):
-    #     if not value[0].isdigit():
-    #         return queryset
-
-    #     all_access = get_settings().all_access.all()
-    #     if not self.request.user.groups.intersection(all_access).exists():
-    #         teachings = ResponsibleModel.objects.get(user=self.request.user).teaching.all()
-    #         classes = get_classes(list(map(lambda t: t.name, teachings)), True, self.request.user)
-    #         queryset = queryset.filter(matricule__classe__in=classes)
-
-    #     if len(value) > 0:
-    #         queryset = queryset.filter(matricule__classe__year=value[0])
-    #         if len(value) > 1:
-    #             queryset = queryset.filter(matricule__classe__letter=value[1].lower())
-    #     return queryset
-
     def no_infos_by(self, queryset, field_name, value):
         if value:
             return queryset.filter(sanction_decision__isnull=False)
