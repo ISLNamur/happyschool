9446f435a9b9cb744aab423438cd02123185e3a0
Supermanu <manueltondeur@gmail.com>
2020-03-12 15:00:16 +0100

    CORE: Dump responsible teaching and classes in each page

---
+++ core/templatetags/user_properties.py
@@ -0,0 +1,39 @@
+# This file is part of HappySchool.
+#
+# HappySchool is the legal property of its developers, whose names
+# can be found in the AUTHORS file distributed with this source
+# distribution.
+#
+# HappySchool is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# HappySchool is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with HappySchool.  If not, see <http://www.gnu.org/licenses/>.
+
+import json
+
+from django import template
+from django.core.exceptions import ObjectDoesNotExist
+
+from core.models import ResponsibleModel
+
+register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def list_user_properties(context):
+    try:
+        responsible = ResponsibleModel.objects.get(user=context['user'])
+        return {
+            "teaching": [t.id for t in responsible.teaching.all()],
+            "classes": [c.id for c in responsible.classe.all()]
+        }
+    except ObjectDoesNotExist:
+        return ""
