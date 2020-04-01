650b096fc9ea18cdb26ac7b1eec1f651fb32d6ef
Supermanu <manueltondeur@gmail.com>
2020-04-01 17:36:09 +0200

    ANNUAIRE: Remove dead code and go object-oriented

--- annuaire/urls.py
+++ annuaire/urls.py
@@ -17,7 +17,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with HappySchool.  If not, see <http://www.gnu.org/licenses/>.
 
-from django.conf.urls import url
 from django.urls import path
 
 from rest_framework.routers import DefaultRouter
@@ -25,24 +24,7 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 urlpatterns = [
-    url(r'^get_list/$', views.get_list, name='get_list'),
-    url(r'^get_people_names/(?P<enseignement>\w+)/(?P<people_type>\w+)/(?P<check_access>[0-9]+)/$', views.get_people_names,
-        name='get_people_names'),
-    url(r'^get_people_names/(?P<enseignement>\w+)/(?P<people_type>\w+)/$', views.get_people_names,
-        name='get_people_names'),
-    url(r'^get_people_names/$', views.get_people_names, name='get_people_names'),
-    url(r'^get_students_matricules/', views.get_students_matricules, name='get_students_matricules'),
-    url(r'^get_students_or_classes/(?P<enseignement>\w+)/(?P<check_access>[0-9]+)/$', views.get_students_or_classes,
-        name='get_students_or_classes'),
-    url(r'^get_students_or_classes/', views.get_students_or_classes, name='get_students_or_classes'),
-    url(r'^get_teachers_emails/', views.get_teachers_emails, name='get_teachers_emails'),
-    url(r'^student/(?P<matricule>[0-9]+)/info/(?P<medical_info>\w+)/(?P<user_info>\w+)', views.info, name='info'),
-    url(r'^student/(?P<matricule>[0-9]+)/info', views.info, name='info'),
-    url(r'^student/(?P<matricule>[0-9]+)/', views.summary, name='summary'),
-    url(r'^teacher/(?P<matricule>[0-9]+)/info/$', views.info_teacher, name='info_teacher'),
-    url(r'^teacher/(?P<id>[0-9]+)/$', views.summary_teacher, name='summary_teacher'),
-    url(r'^get_class_photo_pdf/(?P<classe>[0-9]+)/$', views.get_class_photo_pdf,
-        name="get_class_photo_pdf"),
+    path('get_class_photo_pdf/<int:classe_id>/', views.ClassePhotosView.as_view()),
     path('', views.AnnuaireView.as_view(), name='annuaire'),
     path('get_class_list_excel/<int:classe_id>/', views.ClasseListExcelView.as_view()),
     path('get_class_list_pdf/<int:classe_id>/', views.ClasseListPDFView.as_view()),
--- annuaire/views.py
+++ annuaire/views.py
@@ -25,9 +25,8 @@ from io import BytesIO
 from django.conf import settings
 from django.shortcuts import render
 
-from django.http import Http404, JsonResponse, HttpResponse
+from django.http import HttpResponse
 from django.template.loader import get_template
-from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 from django.views import View
@@ -46,7 +45,7 @@ from z3c.rml import rml2pdf
 from unidecode import unidecode
 
 from core.utilities import get_menu, check_student_photo
-from core.people import People, get_classes, get_years
+from core.people import People, get_classes
 from core.models import StudentModel, ClasseModel, TeachingModel, ResponsibleModel, AdditionalStudentInfo
 from core.serializers import StudentSerializer, ResponsibleSerializer, ClasseSerializer,\
     StudentGeneralInfoSerializer, StudentContactInfoSerializer, StudentMedicalInfoSerializer,\
@@ -56,23 +55,6 @@ from core.views import get_app_settings
 from .models import AnnuaireSettingsModel
 from .serializers import AnnuaireSettingsSerializer
 
-# from core.Person import Person
-# from core.Student import Student
-#
-# from core.StudentManager import StudentManager
-# from core.TeacherManager import TeacherManager
-# from core.EducatorManager import EducatorManager
-# from core.models import StudentLDAP
-# from core.utility import get_new_rows
-#
-# from auth_libreschool.views import get_classes_access
-
-
-from .forms import ChercherEleveForm
-
-# student_man = StudentManager()
-# teacher_man = TeacherManager()
-# educator_man = EducatorManager()
 
 def get_menu_entry(active_app, user):
     return {
@@ -81,209 +63,6 @@ def get_menu_entry(active_app, user):
         "url": "/annuaire/",
         "active": active_app == "annuaire"
     }
-
-groups_with_access = [settings.SYSADMIN_GROUP, settings.TEACHER_GROUP, settings.DIRECTION_GROUP, settings.EDUCATOR_GROUP,
-                      settings.SECRETARY_GROUP, settings.RECEPTION_GROUP, settings.PMS_GROUP]
-
-@login_required
-@user_passes_test(lambda u: u.groups.filter(name__in=groups_with_access), login_url='no_access')
-def get_list(request):
-    # Récuparation des variables GET
-
-    ens = request.GET.get("ens", "all").lower()
-    search = request.GET.get("search", "")
-    matricule = request.GET.get("mat", "")
-    classe = request.GET.get("classe", "")
-    people_type = request.GET.get("type", "student")
-
-    if people_type == 'teacher' and request.user.groups.filter(name__in=[settings.SYSADMIN_GROUP, settings.DIRECTION_GROUP, settings.RECEPTION_GROUP]).exists():
-        return get_teacher_list(request, ens, search)
-
-    students = []
-    if matricule:
-        students = [People().get_student_by_id(int(matricule), [ens])]
-        # students = [student_man.get_person(int(matricule))]
-    elif classe:
-        an = classe[0]
-        classe_letter = classe[1]
-        classe = ClasseModel.objects.get(year=int(an), letter=classe_letter.lower(), teaching__name=ens)
-        students = StudentModel.objects.filter(classe=classe)
-    else:
-        if search.isdigit() and len(search) == 4:
-            # We are looking for a matricule
-            students = People().get_student_by_id(int(matricule))
-        elif search[0].isdigit() :
-            # We are looking for a class
-            classes = ClasseModel.objects.filter(year=int(search[0]))
-            if ens != "all":
-                classes = classes.filter(teaching__name=ens)
-            if len(search) > 1:
-                classes = classes.filter(letter=search[1])
-
-            students = StudentModel.objects.filters(classe=classes)
-        else:
-            students = People().get_students_by_name(search, [ens])
-            # students = student_man.get_people_by_names(ens, search)
-
-    context = {}
-    if classe:
-        context['classe'] = classe.letter
-        context['year'] = classe.year
-        context['enseignement'] = ens
-        students = sorted(students, key=lambda s: unidecode(s.last_name.lower()))
-
-    context['eleves'] = students
-    return render(request, 'annuaire/list.html', context=context)
-
-
-def get_teacher_list(request, ens, search):
-    teachers = []
-    if search != "":
-        # teachers = teacher_man.get_people_by_names(ens, search)
-        teachers = People().get_teachers_by_name(search, [ens])
-
-    return render(request, 'annuaire/list_teacher.html', {'teachers': teachers})
-
-
-@login_required
-@user_passes_test(lambda u: u.groups.filter(name__in=groups_with_access),
-                  login_url='no_access')
-def index(request):
-    chercher_form = ChercherEleveForm()
-
-    # Set a long time expiracy for accueil
-    if request.user.username == 'accueil':
-        request.session.set_expiry(100000000)
-
-    context = {'form': chercher_form, 'new_rows': 0}
-    if request.user.groups.filter(name__in=[settings.SYSADMIN_GROUP, settings.DIRECTION_GROUP, settings.RECEPTION_GROUP]).exists():
-        context['can_search_teachers'] = True
-    return render(request, 'annuaire/index.html', context)
-
-
-@login_required
-def info(request, matricule, medical_info='1', user_info='1'):
-    try:
-        # student = student_man.get_person(matricule)
-        student = People().get_student_by_id(matricule)
-    except StopIteration:
-        raise Http404("Matricule inexistant : " + str(matricule))
-
-    context = {'student': student, 'medical_info': medical_info, 'user_info': user_info }
-
-    return render(request, 'annuaire/eleve_info.html', context=context)
-
-
-@login_required
-def info_teacher(request, matricule):
-    if not request.user.groups.filter(name__in=[settings.SYSADMIN_GROUP, settings.DIRECTION_GROUP, settings.RECEPTION_GROUP]).exists():
-        raise Http404("ID inexistant : " + str(id))
-
-
-    # teacher = teacher_man.get_person(matricule, ens)
-    teacher = People().get_teacher_by_id(matricule)
-    if not teacher:
-        raise Http404("Matricule inexistant : " + str(matricule))
-    context = {'teacher': teacher}
-    if request.user.groups.filter(name__in=[settings.SYSADMIN_GROUP, settings.DIRECTION_GROUP]).exists():
-        context['username_info'] = True
-
-    return render(request, 'annuaire/professeur_info.html', context)
-
-
-@login_required
-@user_passes_test(lambda u: u.groups.filter(name__in=groups_with_access),
-                  login_url='no_access')
-def summary(request, matricule):
-    # eleve = student_man.get_person(matricule)
-    eleve = People().get_student_by_id(matricule)
-    if not eleve:
-        raise Http404("Matricule inexistant : " + str(matricule))
-
-    context = {'eleve': eleve.__dict__}
-    #TODO: infirmerie, appels, dossier_eleve
-    if 'infirmerie' in settings.INSTALLED_APPS:
-        from infirmerie.views import filter_and_order
-        context['infirmerie'] = filter_and_order(request,
-                                             column="matricule",
-                                             data1=eleve.matricule,
-                                             order_by="datetime_encodage",
-                                             order_asc="desc")[:5]
-
-    if 'appels' in settings.INSTALLED_APPS:
-        from appels.views import filter_and_order
-        context['appels'] = filter_and_order(request,
-                                             column="matricule",
-                                             data1=eleve.matricule,
-                                             order_by="datetime_encodage",
-                                             order_asc="desc")[:5]
-
-    return render(request, 'annuaire/eleve.html', context)
-
-
-@login_required
-@user_passes_test(lambda u: u.groups.filter(name__in=[settings.SYSADMIN_GROUP, settings.DIRECTION_GROUP, settings.RECEPTION_GROUP]),
-                  login_url='no_access')
-def summary_teacher(request, id):
-    if not request.user.groups.filter(name__in=[settings.SYSADMIN_GROUP, settings.DIRECTION_GROUP, settings.RECEPTION_GROUP]).exists():
-        raise Http404("ID inexistant : " + str(id))
-
-    # teacher = teacher_man.get_person(id, ens)
-    teacher = People().get_teacher_by_id(id)
-    if not teacher:
-        raise Http404("ID inexistant : " + str(id))
-
-    context = {'teacher': teacher}
-    return render(request, 'annuaire/professeur.html', context)
-
-
-@login_required
-def get_students_names(request, enseignement="all"):
-    return get_people_names(request, enseignement, 'student')
-
-
-@login_required
-@user_passes_test(lambda u: u.groups.filter(name__in=groups_with_access),
-                  login_url='no_access')
-def get_students_matricules(request):
-    suggestions = []
-    filter_str = request.GET.get('query', '')
-    if filter_str == '':
-        return JsonResponse([], safe=False)
-
-    #TODO students matricule
-    # eleves = student_man.get_people(filters=['matricule=' + filter_str + '*'])
-    #
-    #
-    # for e in eleves:
-    #     cn = e.surname
-    #     sn = e.firstname
-    #     matricule = str(e.matricule)
-    #     if not matricule.startswith(filter_str):
-    #         continue
-    #
-    #     classe = e.classe
-    #     suggestions.append({'id': cn + ' ' + sn + ' ' + classe, 'name': e.matricule})
-
-    return JsonResponse(suggestions, safe=False)
-
-
-@login_required
-@user_passes_test(lambda u: u.groups.filter(name__in=groups_with_access),
-                  login_url='no_access')
-def get_students_or_classes(request, enseignement='all', check_access="0"):
-    query = request.GET.get('query', '')
-    enseignement = request.GET.get("ens", enseignement)
-    check_access = check_access == "1"
-    if query[0].isdigit():
-        if len(query) > 1:
-            classes_list = create_classes_list(request, enseignement, check_access=check_access,
-                                               annees=query[0], letters=query[1])
-        else:
-            classes_list = create_classes_list(request, enseignement, check_access=check_access, annees=(query[0]))
-        return format_classes_to_json(classes_list)
-    else:
-        return get_people_names(request, enseignement, people_type='student', check_access=check_access)
 
 
 def create_classes_list(request, enseignement="all", check_access=False, annees="", letters=""):
@@ -296,109 +75,6 @@ def create_classes_list(request, enseignement="all", check_access=False, annees=
         classes = classes.filter(letter=letters.lower())
 
     return list(map(lambda c: str(c), classes))
-
-
-def format_classes_to_json(classes):
-    suggestions = list(map(lambda c: {'id': c.replace(" – ", "-"), 'name': c}, classes))
-    return JsonResponse(suggestions, safe=False)
-
-
-@login_required
-@user_passes_test(lambda u: u.groups.filter(name__in=groups_with_access),
-                  login_url='no_access')
-def get_people_names(request, enseignement='all', people_type='all', formatting='json', check_access=False):
-    people = []
-    query = request.GET.get('query', '')
-
-    if people_type == 'responsible' or people_type == 'all':
-        people += People().get_responsibles_by_name(query, [enseignement])
-
-    if people_type == 'teacher':
-        people += People().get_teachers_by_name(query, [enseignement])
-
-    if people_type == 'educator':
-        people += People().get_educators_by_name(query, [enseignement])
-
-    if people_type == 'student' or people_type == 'all':
-        classe_years = get_classes([enseignement], check_access=True, user=request.user) if check_access else None
-        people += People().get_students_by_name(query, [enseignement], classes=classe_years)
-
-    if formatting == 'json':
-        return format_name_to_json(people, people_type)
-
-    return people
-
-
-@login_required
-@user_passes_test(lambda u: u.groups.filter(name__in=groups_with_access),
-                  login_url='no_access')
-def get_teachers_emails(request, enseignement='secondaire'):
-    query = request.GET.get('query', '')
-    if query == '':
-        return JsonResponse([], safe=False)
-
-    teachers = People().get_teachers_by_name(query, [enseignement])
-    # teachers = teacher_man.get_people_by_names(enseignement, search_string=query)
-    teachers = list(map(lambda t: t.gen_email, teachers))
-    return JsonResponse(teachers, safe=False)
-
-
-def format_name_to_json(people_list, people_type):
-    suggestions = []
-    for p in people_list:
-        display_name = p.fullname
-        if type(p) == StudentModel:
-            if p.classe:
-                display_name += ' ' + p.classe.compact_str
-            if p.teaching.name == 'primaire':
-                display_name += ' ' + 'Prim.'
-
-        suggestions.append({'name': display_name, 'id': p.matricule})
-
-    return JsonResponse(suggestions, safe=False)
-
-
-@login_required
-@user_passes_test(lambda u: u.groups.filter(name__in=groups_with_access),
-                  login_url='no_access')
-def get_class_photo_pdf(request, classe):
-    try:
-        classe = ClasseModel.objects.get(id=classe)
-    except ObjectDoesNotExist:
-        return render(request, 'dossier_eleve/no_student.html')
-    students = StudentModel.objects.filter(classe=classe)
-
-    students = sorted(students, key=lambda s: unidecode(s.last_name.lower()))
-    rows = []
-    student_by_row = 6
-    row = []
-    for i, student in enumerate(students):
-        check_student_photo(student)
-        if i % student_by_row == 0:
-            row = []
-        row.append(student)
-        if i % student_by_row == student_by_row - 1 or len(students) == i + 1:
-            rows.append(row)
-
-    context = {'classe': classe.compact_str, 'list': rows, 'students_numb': len(students)}
-
-    tenures = ResponsibleModel.objects.filter(tenure=classe)
-
-    context['tenures'] = tenures
-
-    context['absolute_path'] = settings.BASE_DIR
-    t = get_template('annuaire/classe.rml')
-    rml_str = t.render(context)
-
-    pdf = rml2pdf.parseString(rml_str)
-    if not pdf:
-        return render(request, 'dossier_eleve/no_student.html')
-    pdf_name = 'classes_%s%s.pdf' % (classe.year, classe.letter)
-
-    response = HttpResponse(content_type='application/pdf')
-    response['Content-Disposition'] = 'filename; filename="' + pdf_name + '"'
-    response.write(pdf.read())
-    return response
 
 
 def get_settings():
@@ -719,6 +395,48 @@ class ClasseListPDFView(LoginRequiredMixin, View):
         if not pdf:
             return render(request, 'dossier_eleve/no_student.html')
         pdf_name = 'classes_%s_%s%s.pdf' % (classe.teaching.name, classe.year, classe.letter)
+
+        response = HttpResponse(content_type='application/pdf')
+        response['Content-Disposition'] = 'filename; filename="' + pdf_name + '"'
+        response.write(pdf.read())
+        return response
+
+
+class ClassePhotosView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        classe_id = kwargs['classe_id']
+        try:
+            classe = ClasseModel.objects.get(id=classe_id)
+        except ObjectDoesNotExist:
+            return render(request, 'dossier_eleve/no_student.html')
+        students = StudentModel.objects.filter(classe=classe)
+
+        students = sorted(students, key=lambda s: unidecode(s.last_name.lower()))
+        rows = []
+        student_by_row = 6
+        row = []
+        for i, student in enumerate(students):
+            check_student_photo(student)
+            if i % student_by_row == 0:
+                row = []
+            row.append(student)
+            if i % student_by_row == student_by_row - 1 or len(students) == i + 1:
+                rows.append(row)
+
+        context = {'classe': classe.compact_str, 'list': rows, 'students_numb': len(students)}
+
+        tenures = ResponsibleModel.objects.filter(tenure=classe)
+
+        context['tenures'] = tenures
+
+        context['absolute_path'] = settings.BASE_DIR
+        t = get_template('annuaire/classe.rml')
+        rml_str = t.render(context)
+
+        pdf = rml2pdf.parseString(rml_str)
+        if not pdf:
+            return render(request, 'dossier_eleve/no_student.html')
+        pdf_name = 'classes_%s%s.pdf' % (classe.year, classe.letter)
 
         response = HttpResponse(content_type='application/pdf')
         response['Content-Disposition'] = 'filename; filename="' + pdf_name + '"'
