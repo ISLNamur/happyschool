138b98b410ace837a294a103740ffb40c24e1f03
Supermanu <manueltondeur@gmail.com>
2020-05-01 18:20:48 +0200

    JS: Copy assets from unofficial app

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,13 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 var webpack = require('webpack');
+const shell = require('child_process').execSync ; 
+
+const src= `./*/assets/*`;
+const dist= `./assets/.`;
+
+shell(`mkdir -p ${dist}`);
+shell(`cp -r ${src} ${dist}`);
 
 module.exports = merge(common, {
   mode: 'production',
