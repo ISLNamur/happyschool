a4fb1ead3f5a08965a64921f2b61be04b745e020
Supermanu <manueltondeur@gmail.com>
2020-05-01 21:57:32 +0200

    JS: Fix shell command for copying external assets

--- webpack.prod.js
+++ webpack.prod.js
@@ -7,7 +7,7 @@ const src= `./*/assets/*`;
 const dist= `./assets/.`;
 
 shell(`mkdir -p ${dist}`);
-shell(`if ls ./*/assets/*  1> /dev/null 2>&1; then; cp -r ${src} ${dist}; fi`);
+shell(`if ls ./*/assets/* 1> /dev/null 2>&1; then cp -r ${src} ${dist}; fi`, {shell: '/bin/bash'});
 
 module.exports = merge(common, {
   mode: 'production',
