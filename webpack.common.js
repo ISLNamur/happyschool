eed6dbd44f7e12219bf370b8449376f49430f76e
Supermanu <manueltondeur@gmail.com>
2020-05-01 18:21:25 +0200

    JS: Fix style issues

--- webpack.common.js
+++ webpack.common.js
@@ -1,99 +1,99 @@
-var path = require('path');
-var webpack = require('webpack');
-var BundleTracker = require('webpack-bundle-tracker');
-const fs = require('fs');
-const VueLoaderPlugin = require('vue-loader/lib/plugin')
-const swCachePlugin = require('sw-cache-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
+var path = require("path");
+var webpack = require("webpack");
+var BundleTracker = require("webpack-bundle-tracker");
+const fs = require("fs");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+const swCachePlugin = require("sw-cache-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 function getEntries () {
-    let apps = fs.readdirSync('./assets/js/')
+    let apps = fs.readdirSync("./assets/js/")
         .filter(
             (file) => file.match(/.*\.js$/)
         )
         .map((file) => {
             return {
                 name: file.substring(0, file.length - 3),
-                path: './assets/js/' + file
-            }
+                path: "./assets/js/" + file
+            };
         }).reduce((memo, file) => {
-            memo[file.name] = [file.path]
+            memo[file.name] = [file.path];
             return memo;
-		}, {})
-	apps['polyfill'] = "@babel/polyfill";
-	return apps;
+        }, {});
+    apps["polyfill"] = "@babel/polyfill";
+    return apps;
 }
 
 module.exports = {
-	context: __dirname,
-	entry: getEntries(),
-	output: {
-		publicPath: '/static/bundles/',
-		path: path.resolve('./static/bundles/'),
-		filename: "[name]-[hash].js"
-	},
+    context: __dirname,
+    entry: getEntries(),
+    output: {
+        publicPath: "/static/bundles/",
+        path: path.resolve("./static/bundles/"),
+        filename: "[name]-[hash].js"
+    },
 
     plugins: [
-		new VueLoaderPlugin(),
-		new BundleTracker({filename: './webpack-stats.json'}),
-		new CopyPlugin([
-			{
-				from: '**', to: '', context: 'assets/sw/'
-			},
-			{
-				from: 'with-async-ittr-min.js', to: 'idb.js', context: 'node_modules/idb/build/iife/'
-			},
-			{
-				from: 'bootstrap.min.css', to: '', context: 'node_modules/bootstrap/dist/css/'
-			}
-		], { logLevel: 'debug' }),
-		new swCachePlugin(
-			{
-			  cacheName: Date.now(),
-			  ignore: [/.*\.map$/],
-			}
-		  )
-	],
-	optimization: {
-		namedModules: true, // NamedModulesPlugin()
-		splitChunks: { // CommonsChunkPlugin()
-			name: 'commons',
-			chunks: 'all',
-			minChunks: 2
-		},
-		noEmitOnErrors: true, // NoEmitOnErrorsPlugin
-		concatenateModules: true //ModuleConcatenationPlugin
-	},
+        new CopyPlugin([
+            {
+                from: "**", to: "", context: "assets/sw/"
+            },
+            {
+                from: "with-async-ittr-min.js", to: "idb.js", context: "node_modules/idb/build/iife/"
+            },
+            {
+                from: "bootstrap.min.css", to: "", context: "node_modules/bootstrap/dist/css/"
+            }
+        ], { logLevel: "info" }),
+        new VueLoaderPlugin(),
+        new BundleTracker({filename: "./webpack-stats.json"}),
+        new swCachePlugin(
+            {
+                cacheName: Date.now(),
+                ignore: [/.*\.map$/],
+            }
+        )
+    ],
+    optimization: {
+        namedModules: true, // NamedModulesPlugin()
+        splitChunks: { // CommonsChunkPlugin()
+            name: "commons",
+            chunks: "all",
+            minChunks: 2
+        },
+        noEmitOnErrors: true, // NoEmitOnErrorsPlugin
+        concatenateModules: true //ModuleConcatenationPlugin
+    },
 
-	resolve: {
-		alias: {
-			vue: 'vue/dist/vue.common.js',
-			assets: path.resolve(__dirname, 'assets/')
-		}
-	},
+    resolve: {
+        alias: {
+            vue: "vue/dist/vue.common.js",
+            assets: path.resolve(__dirname, "assets/")
+        }
+    },
 
-	module: {
-		rules: [
-			{
-				test: /\.vue$/,
-				loader: 'vue-loader',
-				options: {
-					loaders: {
-					}
-					// other vue-loader options go here
-				}
-			},
-			{
-				test: /\.css$/,
-				use: [
-					{ loader: "style-loader" },
-					{ loader: "css-loader" }
-				]
-			},
-			{
-				test: /\.js$/,
-				loader: 'babel-loader',
-			}
-		]
-	}
-}
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: "vue-loader",
+                options: {
+                    loaders: {
+                    }
+                    // other vue-loader options go here
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: "style-loader" },
+                    { loader: "css-loader" }
+                ]
+            },
+            {
+                test: /\.js$/,
+                loader: "babel-loader",
+            }
+        ]
+    }
+};
