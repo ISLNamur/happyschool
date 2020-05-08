009c4982cb1773aa8c702e13222ab4516647ddae
Supermanu <manueltondeur@gmail.com>
2020-05-08 13:44:30 +0200

    JS: Move external assets before bundles generation

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,13 +1,14 @@
-const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
-var webpack = require('webpack');
 const shell = require('child_process').execSync ; 
 
 const src= `./*/assets/*`;
 const dist= `./assets/.`;
 
 shell(`mkdir -p ${dist}`);
-shell(`if ls ./*/assets/* 1> /dev/null 2>&1; then cp -r ${src} ${dist}; fi`, {shell: '/bin/bash'});
+shell(`if ls ./*/assets/* 1> /dev/null 2>&1; then cp -r ${src} ${dist}; fi`, { shell: '/bin/bash' });
+
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+var webpack = require('webpack');
 
 module.exports = merge(common, {
   mode: 'production',
