cedae1a6b767f7d3da9e3edbf0f7ba04b53c41e1
Supermanu <manueltondeur@gmail.com>
2021-05-27 10:50:04 +0200

    SCHEDULE_CHANGE: Fix schedule change id setting

--- schedule_change/serializers.py
+++ schedule_change/serializers.py
@@ -51,6 +51,7 @@ class ScheduleChangePlaceSerializer(serializers.ModelSerializer):
 
 
 class ScheduleChangeSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(label='ID', read_only=False)
     send_email_general = serializers.BooleanField(write_only=True, required=False)
     send_email_substitute = serializers.BooleanField(write_only=True, required=False)
     classes = FlatArrayField()
@@ -69,7 +70,12 @@ class ScheduleChangeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = ScheduleChangeModel
-        fields = '__all__'
+        fields = [
+            "id", "change", "category", "date_change", "time_start", "time_end",
+            "send_email_general", "send_email_substitute", "teachers_replaced",
+            "teachers_replaced_id", "teachers_substitute", "teachers_substitute_id",
+            "classes", "place", "comment", "hide_for_students",
+            ]
         read_only_fields = ('datetime_created', 'datetime_modified', 'user', 'created_by',)
 
 
