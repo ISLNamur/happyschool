6c4bc3107a86ca30f87080414dded0f69d84d660
Supermanu <manueltondeur@gmail.com>
2021-12-02 13:59:25 +0100

    SCRIPTS: Use pipenv for upgrade script

--- scripts/update.sh
+++ scripts/update.sh
@@ -4,7 +4,8 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd $parent_path
 cd ..
 git pull
-pip3 install --user -r requirements.txt
-python3 manage.py migrate
+rm Pipfile.lock
+pipenv install
+pipenv run ./manage.py migrate
 npm install
 ./node_modules/.bin/webpack --config webpack.prod.js
