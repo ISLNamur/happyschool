5c02fd1c7e21e20ba239ff8bbffa36ef4dfc7d37
Supermanu <manueltondeur@gmail.com>
2022-06-16 15:08:05 +0200

    HAPPYSCHOOL: Bump to Django 4.0

--- happyschool/asgi.py
+++ happyschool/asgi.py
@@ -24,8 +24,12 @@ defined in the ASGI_APPLICATION setting.
 
 import os
 import django
-from channels.routing import get_default_application
+from channels.routing import ProtocolTypeRouter
+from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "happyschool.settings")
 django.setup()
-application = get_default_application()
+
+application = ProtocolTypeRouter({
+  "http": get_asgi_application(),
+})
