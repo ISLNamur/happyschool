c3d8ba38ea7f50e1e69d44e751ef1fa94a4a84f0
Patrick Jacquemin <patrickjacquemin@gmail.com>
2023-06-16 16:07:30 +0200

    set_extension.sh fixed : use the right db name otherwise unaccent will not work

--- docker/postgres/set_extension.sh
+++ docker/postgres/set_extension.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d 'template1' -c 'CREATE EXTENSION unaccent;'
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$POSTGRES_DB" -c 'CREATE EXTENSION unaccent;'
