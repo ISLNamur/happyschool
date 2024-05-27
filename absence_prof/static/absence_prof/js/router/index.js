0c6854e1004b7d7a0da08e0157a13aaf201620ad
Supermanu <manueltondeur@gmail.com>
2024-05-27 14:40:31 +0200

    ABSENCE_PROF: Migrate to vue3

---
+++ absence_prof/static/absence_prof/js/router/index.js
@@ -0,0 +1,45 @@
+// This file is part of Happyschool.
+//
+// Happyschool is the legal property of its developers, whose names
+// can be found in the AUTHORS file distributed with this source
+// distribution.
+//
+// Happyschool is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Happyschool is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with Happyschool.  If not, see <http://www.gnu.org/licenses/>.
+
+import AbsenceProf from "../absence_prof.vue";
+import EditAbsenceProf from "../editAbsenceProf.vue";
+
+import { createRouter, createWebHashHistory } from "vue-router";
+
+const router = createRouter({
+    routes: [
+        {
+            path: "/",
+            component: AbsenceProf,
+        },
+        {
+            path: "/edit/:id/",
+            component: EditAbsenceProf,
+            props: true
+        },
+        {
+            path: "/new/",
+            component: EditAbsenceProf,
+            props: true
+        },
+    ],
+    history: createWebHashHistory(),
+});
+
+export default router;
