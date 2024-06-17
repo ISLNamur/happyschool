5726de18656fa8d11fef7976194fbe4f73d97661
Supermanu <manueltondeur@gmail.com>
2024-06-17 11:17:43 +0200

    ALL: Remove unused config file

--- webpack.dev.js
+++
@@ -1,8 +0,0 @@
-const { merge } = require("webpack-merge");
-const common = require("./webpack.common.js");
-const dev = {
-    mode: "development",
-    devtool: "source-map",
-};
-
-module.exports = merge(common, dev);
