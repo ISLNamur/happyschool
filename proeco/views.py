54192b690000c0d7eaab787fbee8513e08363ef8
Supermanu <manueltondeur@gmail.com>
2025-01-30 22:18:50 +0100

    PROECO: Add documentation

--- proeco/views.py
+++ proeco/views.py
@@ -35,30 +35,63 @@ class ExportStudentSelectionAPI(APIView):
     ]
 
     def get(self, request, format=None, **kwargs):
+        """
+        Defines the API endpoint to fetch student selections.
+
+        :return: A response object with the generated file.
+        """
+
+        # Get a list of students by matricules
         matricules = set(self._get_student_list(request, kwargs))
+
+        # Initialize template model
         template_model = TemplateSelectionModel.objects.first()
+
+        # Generate text content with student selections
         matricules_text = ["MATRICS%i=%i=" % (i + 1, a) for (i, a) in enumerate(matricules)]
         text = template_model.template.replace("{matricules}", "\n".join(matricules_text))
+
+        # Create response object with attachment content and file name
         response = HttpResponse(text, content_type="text/plain")
         response["Content-Disposition"] = (
             f'attachment; filename="{self._format_file_name(request, **kwargs)}"'
         )
+
         return response
 
-    def _get_student_list(self, request, **kwargs):
-        """Get a list of student by their matricules."""
-        pass
 
-    def _format_file_name(self, request, **kwargs):
-        return "Pref_NOMS_.TXT"
+def _get_student_list(self, request, **kwargs):
+    """
+    Fetches a list of students by their matricules.
+
+    :param request: HTTP request object
+    :return: A set of student matricules.
+    """
+
+    pass
+
+
+def _format_file_name(self, request, **kwargs):
+    return "Pref_NOMS_.TXT"
 
 
 def proeco_write(
     app: str, method: str, payload: list, person: str, user: User, teaching: TeachingModel
 ):
+    """
+    Create and save a new ProEcoWriteModel instance, then apply the task to Celery's queue.
+
+    :param app: The name of the django application.
+    :param method: The name of the libreschoolfdb method.
+    :param payload: The data to be written.
+    :param person: The type of the person involved (student or teacher).
+    :param user: The current user.
+    :param teaching: A model representing a teaching model from example_app import proeco_write
+    """
     write = ProEcoWriteModel(
         app=app, method=method, payload=payload, person=person, user=user, teaching=teaching
     )
     write.save()
 
+    # Asynchronously process the write operation with a 2-second countdown
     task_write_proeco.apply_async(args=(write.id,), countdown=2)
