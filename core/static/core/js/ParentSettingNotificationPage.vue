0b57a7a12558456dda7fedc7535756d0cb4b840f
Supermanu <manueltondeur@gmail.com>
2025-09-11 11:14:31 +0200

    CORE: Fix style issue

--- core/static/core/js/ParentSettingNotificationPage.vue
+++ core/static/core/js/ParentSettingNotificationPage.vue
@@ -21,51 +21,56 @@
     <BToastOrchestrator />
     <BContainer>
         <BCard class="mt-2">
-        <BRow>
-            <BCol>
-                <h1 class="text-center">Paramètres de notification</h1>
-            </BCol>
-        </BRow>
-        <BRow>
-            <BCol>
-                <h2>Retards et exclusions</h2>
-                <p>
-                    Si vous le décidez, vous avez la possibilité de recevoir un courriel chaque semaine un récapitulatif des retards justifiés et injustifiés ainsi que des exclusions de cours.
-                    S'il n'y a aucun retard ou aucune exclusion, aucun courriel ne sera envoyé.
-                </p>
-            </BCol>
-        </BRow>
-        <BRow>
-            <BCol>
-                Élève concerné : <strong>{{ student }}</strong>
-            </BCol>
-        </BRow>
-        <BRow v-for="email, i in emails" :key="i">
-            <BCol class="mt-4">
-                <label>{{ email }}</label>
-                <BFormCheckbox v-model="notif[i]">
-                    Envoyer des notifications par courriel toutes les semaines.
-                </BFormCheckbox>
-            </BCol>
-        </BRow>
-        <BRow class="mt-3">
-            <BCol>
-                <BButton
-                    variant="primary"
-                    :loading="sending"
-                    :disabled="sending"
-                    @click="sendSettings"
-                >
-                    Envoyer
-                </BButton>
-            </BCol>
-        </BRow>
+            <BRow>
+                <BCol>
+                    <h1 class="text-center">
+                        Paramètres de notification
+                    </h1>
+                </BCol>
+            </BRow>
+            <BRow>
+                <BCol>
+                    <h2>Retards et exclusions</h2>
+                    <p>
+                        Si vous le décidez, vous avez la possibilité de recevoir un courriel chaque semaine un récapitulatif des retards justifiés et injustifiés ainsi que des exclusions de cours.
+                        S'il n'y a aucun retard ou aucune exclusion, aucun courriel ne sera envoyé.
+                    </p>
+                </BCol>
+            </BRow>
+            <BRow>
+                <BCol>
+                    Élève concerné : <strong>{{ student }}</strong>
+                </BCol>
+            </BRow>
+            <BRow
+                v-for="email, i in emails"
+                :key="i"
+            >
+                <BCol class="mt-4">
+                    <label>{{ email }}</label>
+                    <BFormCheckbox v-model="notif[i]">
+                        Envoyer des notifications par courriel toutes les semaines.
+                    </BFormCheckbox>
+                </BCol>
+            </BRow>
+            <BRow class="mt-3">
+                <BCol>
+                    <BButton
+                        variant="primary"
+                        :loading="sending"
+                        :disabled="sending"
+                        @click="sendSettings"
+                    >
+                        Envoyer
+                    </BButton>
+                </BCol>
+            </BRow>
         </BCard>
     </BContainer>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 import { useToastController } from "bootstrap-vue-next";
 
@@ -78,9 +83,12 @@ export default {
     },
     data: function () {
         return {
+            // eslint-disable-next-line no-undef
             student: student,
             contact: null,
+            // eslint-disable-next-line no-undef
             emails: emails,
+            // eslint-disable-next-line no-undef
             notif: new Array(emails.length),
             sending: false,
         };
@@ -96,22 +104,24 @@ export default {
                 }
             });
             
+            // eslint-disable-next-line no-undef
             axios.post(`/core/api/parent_settings/${uuid}/`, data={ emails: data }, token)
                 .then(() => {
                     this.sending = false;
                     this.show({
                         body: "Les informations ont été sauvées.",
                         variant: "success",
-                    })
+                    });
                 }).catch(() => {
                     this.show({
                         body: "Une erreur est survenue. Merci de réessayer plus tard ou de prévenir le service informatique de l'école.",
                         variant: "danger",
-                    })
-                })
+                    });
+                });
         }
     },
     mounted: function () {
+        // eslint-disable-next-line no-undef
         axios.get(`/core/api/parent_settings/${uuid}/`)
             .then(resp => {
                 console.log(resp.data);
@@ -121,7 +131,7 @@ export default {
                         this.notif[i] = true;
                     }
                 });
-            })
+            });
     }
 };
 </script>
